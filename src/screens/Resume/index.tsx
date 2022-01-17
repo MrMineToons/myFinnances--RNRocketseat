7e9feda00a57555feea541a68cbddfc5605926fa
MMT <mrminetoons@gmail.com>
2022-01-17 14:55:40 -0400

    Finalizado o modulo de Async Storage

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,4 +1,4 @@
-import React , { useEffect, useState, useCallback } from 'react';
+import React , { useState, useCallback } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { ActivityIndicator } from 'react-native';
 import { VictoryPie } from 'victory-native';
